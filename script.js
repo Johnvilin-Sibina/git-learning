29973ba926bfd2b5ebfb7259ad26f75088fe51a5
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-02-13 12:30:43 +0530

    Problem Solved by Mentor.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Find the missing number in an array.
 //Solution:
-let arr=[1,2,3,5];
+/*let arr=[1,2,3,5];
 for(let i=0;i<arr.length-1;i++){
     let diff=arr[i+1]-arr[i]
     if(diff==1){
@@ -8,5 +8,12 @@ for(let i=0;i<arr.length-1;i++){
     }
     else{
         console.log(arr[i]+1);
+    }
+}*/
+let arr=[1,2,3,5];
+for(let i=0;i<arr.length;i++){
+    if(arr[i]+1!=arr[i+1]){
+    console.log(arr[i]+1);
+    break;
     }
 }
