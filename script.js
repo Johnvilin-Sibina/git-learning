7d8b88092e5059bdc29d9151effc730726179059
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-02-23 12:26:20 +0530

    Age-Calculator task done in class

--- script.js
+++ script.js
@@ -1,84 +1,62 @@
-// getElementsByClassName:
-var res = document.getElementsByClassName("main");
-console.log(res);
-// getElementsByTagName
-var res = document.getElementsByTagName("p");
-console.log(res);
-// querySelector:
-var res = document.querySelector("p");
-console.log(res);
-// querySelectorAll:
-var res = document.querySelectorAll("p");
-console.log(res);
-// addEventListener:
+var date = document.createElement("input");
+date.setAttribute("type","date");
+date.id = "dob";
+
 var button = document.createElement("button");
 button.setAttribute("type","button");
-button.innerHTML = "Click Here";
-button.addEventListener("click",foo);
-document.body.append(button);
+button.className = "btn btn-primary";
+button.innerHTML = "Click Me";
+button.addEventListener("click",date_data);
 
-function foo(){
-    console.log("Hello World")
+document.body.append(date,button);
+
+function date_data(){
+   var input = document.getElementById("dob").value;
+   var input_date = new Date(input);
+   var currentdate = new Date();
+   
+
+   var millisecdiff = parseInt(currentdate.getTime()-input_date.getTime())
+   
+   var secondsdiff = Math.floor(millisecdiff/1000);
+    
+   var minutesdiff = Math.floor(secondsdiff/60);
+   
+
+   var hoursdiff = Math.floor(minutesdiff/60);
+   
+
+   var daydiff = Math.floor(hoursdiff/24);
+   
+
+   var yeardiff = currentdate.getFullYear() - input_date.getFullYear();
+   
+   
+   var monthdiff = (yeardiff*12)+(currentdate.getMonth()-input_date.getMonth());
+   
+
+   var p1 = document.createElement("p");
+   p1.innerHTML = `Years: ${yeardiff}`;
+
+   var p2 = document.createElement("p");
+   p2.innerHTML = `Months: ${monthdiff}`;
+
+   var p3 = document.createElement("p");
+   p3.innerHTML = `Days: ${daydiff}`;
+
+   var p4 = document.createElement("p");
+   p4.innerHTML = `Hours: ${hoursdiff}`;
+
+   var p5 = document.createElement("p");
+   p5.innerHTML = `Minutes: ${minutesdiff}`;
+
+   var p6 = document.createElement("p");
+   p6.innerHTML = `Seconds: ${secondsdiff}`;
+
+   var p7 = document.createElement("p");
+   p7.innerHTML = `MilliSeconds: ${millisecdiff}`;
+   
+   document.body.append(p1,p2,p3,p4,p5,p6,p7);
+   
 }
 
-// append vs append
-var div = document.createElement("div");
-var p = document.createElement("p");
-var span = document.createElement("span");
-div.append(p,span);
-document.body.append(div);
-var div = document.createElement("div");
-var h1 = document.createElement("h1");
-div.appendChild(h1);
-document.body.append(div);
-
-// Alert
-var button1 = document.createElement("button");
-button1.setAttribute("type","button");
-button1.innerHTML = "Click Here";
-button1.addEventListener("click",foo1);
-document.body.append(button1);
-
- function foo1(){
-    alert ("Hello World")
- }
-
- // prompt
-var button2 = document.createElement("button");
-button2.setAttribute("type","button");
-button2.innerHTML = "Click Here";
-button2.addEventListener("click",foo2);
-document.body.append(button2);
-
- function foo2(){
-    var res = parseInt(prompt("Enter the age of the student:"));
-    console.log(res);
- }
- // confirm
-var button3 = document.createElement("button");
-button3.setAttribute("type","button");
-button3.innerHTML = "Click Here";
-button3.addEventListener("click",foo3);
-document.body.append(button3);
-
-function foo3(){
-    var res = confirm("Do you want to close it!!")
-    console.log(res);
- }
-
- // How to dispaly data entered by user inside the body of HTML document?
-
-var button4 = document.createElement("button");
-button4.setAttribute("type","button");
-button4.innerHTML = "Click Here";
-button4.addEventListener("click",foo4);
-document.body.append(button4);
-
-
- function foo4(){
-    var res = parseInt(prompt("Enter the age of the student:"));
-    var span = document.createElement("span");
-    span.innerHTML = `The age of the student is ${res}`;
-    document.body.append(span);
-
- }
