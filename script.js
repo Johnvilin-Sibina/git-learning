efe93c74ae8fd01d946e524ef753aa83f75b9e7e
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-02-28 12:36:15 +0530

    Fetch

--- script.js
+++ script.js
@@ -1,120 +1,65 @@
-// Promise:
+var result = fetch ("https://restcountries.com/v3.1/all")
+.then((data)=>data.json()).then((data1)=>console.log(data1));
 
-// var p1 = new Promise((resolve,reject)=>resolve("This is resolved"));
-// console.log(p1);
+var res = fetch ("https://restcountries.com/v3.1/all")
+.then((data)=>data.json()).then((data1)=>bar(data1));
 
-// var p1 = new Promise((resolve,reject)=>reject("This is rejected"));
-// console.log(p1);
+var container = document.createElement("div");
+container.className = "container"
 
-// var p1 = new Promise((resolve,reject)=>setTimeout(()=>resolve([2,13,14,15]),1000));
-// console.log(p1);
+var row = document.createElement("div");
+row.className = "row"
 
-// function foo(){
-//    return new Promise((resolve,reject)=>setTimeout(()=>resolve([12,13,14,15]),1000));
-// }
 
-// var p1 = foo();
-// console.log(p1);
+function bar(data1){
+   for(var i=0;i<data1.length;i++){
+      var col = document.createElement("div");
+      col.className = "col-md-4"
 
-// function foo(){
-//    return new Promise((resolve,reject)=>setTimeout(()=>resolve([12,13,14,15]),1000));
-// }
-
-// var p1 = foo();
-// p1.then((data)=>console.log(data))
-
-// function foo(){
-//    return new Promise((resolve,reject)=>setTimeout(()=>resolve([12,13,14,15]),1000));
-// }
-// function bar(){
-//    return new Promise((resolve,reject)=>setTimeout(()=>reject("This is rejected"),1000));
-// }
-
-// var p1 = bar();
-// p1.catch((error)=>console.log(error));
-
-// var p1 =new Promise((resolve,reject)=>{
-//    resolve(setTimeout(()=>console.log("Hello! It is  Resolved"),2000))
-//    reject(setTimeout(()=>console.log("Hello! It is rejected"),2000))
-// })
-// p1.then((data)=>console.log(data)).catch((error)=>console.log(error))
-
-// var p1 =new Promise((resolve,reject)=>{
-//    resolve("Hello! It is Resolved")
-//    reject("Hello! It is rejected")
-   
-// })
-// p1.then((data)=>console.log(data)).catch((error)=>console.log(error))
-
-// var p1 =new Promise((resolve,reject)=>{
-//    var age = parseInt(prompt("Enter your age"));
-//    if(age>=18){
-//       return resolve("You are eligible to vote")
-//    }
-//    else{
-//       return reject("You are not eligible to vote")
-//    }
-// })
-// age.then((data)=>console.log(data)).catch((error)=>console.log(error))
-
-// Chaining:
-
-// function mul(num){
-//    return new Promise((resolve,reject)=>setTimeout(()=>resolve(num*2,1000)))
-// }
-// mul(5).then((data)=>{
-//    console.log(data);
-//    return mul(data)
-// }).then((data1)=>{
-//    console.log(data1)
-//    return mul(data1)
-// }).then((data2)=>console.log(data2))
-
-// function mul(num){
-//    return new Promise((resolve,reject)=>setTimeout(()=>resolve(num*2,1000)))
-// }
-// function bar(num){
-//    return new Promise((resolve,reject)=>setTimeout(()=>reject(`This is rejected state ${num*2}`),1000))
-// }
-// mul(5).then((data)=>{
-//    console.log(data);
-//    return mul(data)
-// }).then((data1)=>{
-//    console.log(data1)
-//    return bar(data1)
-// }).then((data2)=>console.log(data2)).catch((error)=>console.log(error))
-
-// How to handle multiple promises?
-
-var p1 =new Promise((resolve,reject)=>{
-   if(true){
-      setTimeout(()=>resolve("P1 is resolved!!"),3000)
+      col.innerHTML = `<div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Name: ${data1[i].name.common}</h5>
+        <h6></h6>
+        <p class="card-text">Capital: ${data1[i].capital ? data1[i].capital : "Capital not found"}</p>
+      </div>
+    </div>`
+    row.append(col);
+    container.append(row);
+    document.body.append(container);
    }
-   else{
-      setTimeout(()=>reject("P1 is rejected!!"),2000)
-   }
-})
-var p2 =new Promise((resolve,reject)=>{
-   if(true){
-      setTimeout(()=>resolve("P2 is resolved!!"),2000)
-   }
-   else{
-      setTimeout(()=>reject("P2 is rejected!!"),2000)
-   }
-})
+}
 
-var p3 =new Promise((resolve,reject)=>{
-   if(true){
-      setTimeout(()=>resolve("P3 is resolved!!"),2000)
-   }
-   else{
-      setTimeout(()=>reject("P3 is rejected!!"),2000)
-   }
-})
 
-// p1.then((data)=>console.log(data)).catch((error)=>console.log(error))
-// p1.then((data1)=>console.log(data1)).catch((error)=>console.log(error))
-// p1.then((data2)=>console.log(data2)).catch((error)=>console.log(error))
-// Promise.all([p1,p2,p3]).then((data)=>console.log(data)).catch((error)=>console.log(error))
-Promise.allSettled([p1,p2,p3]).then((data)=>console.log(data)).catch((error)=>console.log(error))
+// function bar(data1){
+//    for(var i=0;i<data1.length;i++){
+//       var res = data1[i].latlng;
+//       foo(...res)
+//    }  
+// }
+    
+// function foo(lat,lon){
+//    var final_res = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5eb779a14d85a8b363191098dc26c5ef`)
+//    .then((data2)=>data2.json()).then((data3)=>console.log(data3.main.temp))
+// } 
+
+// var res1 = fetch ("https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json").then((data)=>data.json()).then((data1)=>console.log(data1));
+
+// var  res1 = fetch ("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json").then((data)=>data.json()).then((data1)=>console.log(data1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
