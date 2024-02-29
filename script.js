d009853899550ddef47a5b32653ba229e3db8bbb
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-02-29 12:48:28 +0530

    Covid task giv in class.

--- script.js
+++ script.js
@@ -1,94 +1,3 @@
-// var res = fetch("https://restcountries/v3.1/all")
-// .then((data)=>data.json()).then((data1)=>console.log(data1))
-
-// Async function always returns a promise object
-
-/* async function foo(){
-   return "Hello World"
-}
-console.log(foo()) */
-
-// Basic example of using await
-
-/* function bar(){
-   return new Promise((resolve,reject)=>resolve("This is resolved!!"))
-}
-async function foo(){
-   let res = await bar()
-   console.log(res);
-}
-console.log(foo())*/
-
-/* async function foo(){
-   let res = await fetch("https://restcountries/v3.1/all")
-   let res1 = await res.json();
-   console.log(res1);
-}
-console.log(foo()) 
-
-/* async function foo(){
-   let res = await fetch("https://data.covid19india.org/v4/min/data.min.json");
-   let res1 = await res.json()
-   console.log(res1)
-}
-foo() */
-//Dnme by me
-/* async function bar(){
-   var res = await fetch("https://restcountries.com/v3.1/all")
-   var res1 = await res.json();
-for(var i =0;i<res1.length;i++){
-    var result = res1[i].latlng
-    foo(...result)
-}
-}
-
-async function foo(lat,lon){
-   var final_res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5eb779a14d85a8b363191098dc26c5ef`)
-   var final_result = await final_res.json()
-   console.log(final_result.main.temp)
-   }
-   bar() */
-
-   // By mentor
-
-/* async function getdata(){
-   var res = await fetch("https://restcountries.com/v3.1/all")
-   var res1 = await res.json()
-   for(var i=0;i<res1.length;i++){
-      var latitude = res1[i].latlng
-      putdata(...latitude)
-   }
-
-}
-async function putdata(lat,lon){
-   var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5eb779a14d85a8b363191098dc26c5ef`)
-   var res1 = await res.json()
-   console.log(res1.main.temp)
-}
-getdata() */
-
-// try-catch (handling error or rejected state)
-
-/* function getlist(){
-   return new Promise((resolve,reject)=>reject("This is rejected"))
-}
-
-function getdata(){
-   return new Promise((resolve,reject)=>resolve("This is resolved"))   
-}
-
-async function foo(){
-   try{
-      let data = await getdata()
-      console.log(data);
-      let res = await getlist()
-      console.log(`This is statement after the error block ${res}`)
-   } catch(error){
-      console.log(error)
-   }
-}
-foo() */
-
 async function getdata(){
    var res = await fetch("https://data.covid19india.org/v4/min/data.min.json");
    var data = await res.json();
@@ -106,7 +15,7 @@ var row = document.createElement("div");
 row.className = "row"
 var col = document.createElement("div");
 col.className = "col-md-12"
-col.innerHTML = `<div class="card bg-light mb-3" style="max-width: 18rem;">
+col.innerHTML = `<div class="card" style="max-width: 18rem;">
 <div class="card-header">Chennai</div>
 <div class="card-body">
   <h5 class="card-title">Confirmed: ${confirmed}</h5>
